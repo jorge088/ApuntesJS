b67b4f4f7588a42356862b49f715ed61ad1800aa
Jorge Mendez <mendezjorge98@gmail.com>
2022-06-17 20:11:23 -0300

    added home page content and styles

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const navLogo = document.querySelector('#navLogo');
+const mainContent = document.querySelector('#main');
 
 navLogo.addEventListener('click',(e) => {
     e.preventDefault();
+    scrollTo(0,mainContent.offsetTop - 70)
 });
